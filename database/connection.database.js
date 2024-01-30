035084e15fb74d0dc8a32d49293a9f76c1a26c76
LuisBurbano <laburbano1@espe.edu.ec>
2024-01-29 22:45:06 -0500

    arreglado uri

--- database/connection.database.js
+++ database/connection.database.js
@@ -1,6 +1,6 @@
 
 import { MongoClient, ServerApiVersion } from  'mongodb';
-const uri = "mongodb+srv://adminPizza:6i82PjLNVh9Xt4M@pizzeria.dwwqof4.mongodb.net/?retryWrites=true&w=majority";
+const uri = import.meta.env.URI_MONGO;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 export const client = new MongoClient(uri, {
